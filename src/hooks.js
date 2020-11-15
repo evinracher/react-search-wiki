72b0edd243b26626c3dacfb18d6d0cc1fcbe5765
evinracher <eevinley@gmail.com>
2020-11-15 15:34:26 -0500

    Cancelling request

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 export const useSearch = (query) => {
@@ -9,11 +9,27 @@ export const useSearch = (query) => {
     error: ''
   });
 
+  const cancelToken = useRef(null);
+
   useEffect(() => {
-    axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${query}`)
+    if (query === '') {
+      return;
+    }
+
+    if (cancelToken.current) {
+      console.log('Canceling the request');
+      cancelToken.current.cancel();
+    }
+
+    cancelToken.current = axios.CancelToken.source();
+
+    axios.get(
+      `https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${query}`,
+      { cancelToken: cancelToken.current.token }
+    )
       .then(function (response) {
         const parsedResponse = [];
-        for(let i = 0; i < response.data[1].length; i++) {
+        for (let i = 0; i < response.data[1].length; i++) {
           parsedResponse.push({
             id: response.data[3][i],
             label: response.data[1][i]
@@ -27,6 +43,11 @@ export const useSearch = (query) => {
         });
       })
       .catch(function (error) {
+        // If it was cancelled, just return
+        if (axios.isCancel(error)) {
+          console.log('Request was cancelled');
+          return;
+        }
         // handle error
         console.error(error);
         setState({
