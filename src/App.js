3f0434a9a495ac70665c5b4cbf6f3f6ff2713f8a
evinracher <eevinley@gmail.com>
2020-11-15 15:04:25 -0500

    Adding the backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import ReactAutocomplete from 'react-autocomplete';
+import axios from 'axios';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${value}`)
+      .then(function (response) {
+        const parseResponse = [];
+        for(let i = 0; i < response.data[1].length; i++) {
+          parseResponse.push({
+            id: response.data[3][i],
+            label: response.data[1][i]
+          });
+        }
+
+        setItems(parseResponse);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }, [value]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ReactAutocomplete
+      items={items}
+      shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+      getItemValue={item => item.label}
+      renderItem={(item, highlighted) =>
+        <div
+          key={item.id}
+          style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {item.label}
+        </div>
+      }
+      value={value}
+      onChange={e => setValue(e.target.value)}
+      onSelect={value => setValue(value)}
+    />
   );
 }
 
