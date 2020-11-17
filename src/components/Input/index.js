6430662b51769149f89f6b55a8a66c60967b6afc
evinracher <eevinley@gmail.com>
2020-11-16 22:18:01 -0500

    Adding alternative way

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './styles.scss';
 import PropTypes from 'prop-types';
 
+// You can also put default values like this
+// const Input = ({ placeholder = 'Input search query', ...rest }) => {
 const Input = ({ placeholder, ...rest }) => {
-  return (<input className="input-field" placeholder={placeholder} {...rest}/>)
+  return (<input className="input-field" placeholder={placeholder} {...rest} />)
 }
 
+// This can get deprecated
 Input.defaultProps = {
   placeholder: 'Input search query',
-}
+};
 
 export default Input;
