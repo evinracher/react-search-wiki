5f43e3883dd222d18e66ec771dda425e6b6f9ffe
evinracher <eevinley@gmail.com>
2020-11-16 21:45:52 -0500

    Done with tests

--- src/App.js
+++ src/App.js
@@ -4,20 +4,16 @@ import Search from './pages/Search';
 import NotFound from './pages/NotFound';
 
 import {
-  BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/search" component={Search}/>
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/search" component={Search} />
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App;
